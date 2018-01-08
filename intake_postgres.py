375de0ca56cc6565f18f77190cef837884d8870b
gbrener <gbrener@continuum.io>
2018-01-08 09:15:08 -0600

    Make changes for parity with intake

--- intake_postgres.py
+++ intake_postgres.py
@@ -34,46 +34,19 @@ class PostgresSource(base.DataSource):
 
         super(PostgresSource, self).__init__(container='dataframe', metadata=metadata)
 
-    def _get_dataframe(self):
+    def _get_schema(self):
         if self._dataframe is None:
-            # index_col = pg_kwargs.get('index_col', 'index')
-            # self._dataframe = dd.read_sql_table(self._sql_expr, self._uri, index_col, **self._pg_kwargs)
             engine = sa.create_engine(self._uri)
             self._dataframe = pd.read_sql_query(self._sql_expr, engine)
-
             dtypes = self._dataframe.dtypes
-            self.datashape = None
-            self.dtype = list(zip(dtypes.index, dtypes))
-            self.shape = (len(self._dataframe),)
-            self.npartitions = 1 # self._dataframe.npartitions
+        return base.Schema(datashape=None,
+                           dtype=list(zip(dtypes.index, dtypes)),
+                           shape=(None,),
+                           npartitions=1,
+                           extra_metadata={})
 
+    def _get_partition(self, _):
         return self._dataframe
 
-    def discover(self):
-        self._get_dataframe()
-        return dict(datashape=self.datashape, dtype=self.dtype, shape=self.shape, npartitions=self.npartitions)
-
-    def read(self):
-        return self._get_dataframe() # .compute()
-
-    # def read_chunked(self):
-    #     df = self._get_dataframe()
-    #
-    #     for i in range(df.npartitions):
-    #         yield df.get_partition(i).compute()
-    #
-    # def read_partition(self, i):
-    #     df = self._get_dataframe()
-    #     return df.get_partition(i).compute()
-    #
-    # def to_dask(self):
-    #     return self._get_dataframe()
-
-    def close(self):
+    def _close(self):
         self._dataframe = None
-
-    def __getstate__(self):
-        return self._init_args
-
-    def __setstate__(self, state):
-        self.__init__(**state)
--- tests/test_intake_postgres.py
+++ tests/test_intake_postgres.py
@@ -61,7 +61,7 @@ def test_discover(engine, table_name, csv_fpath):
     source = p.open(DB_URI, 'select * from '+table_name)
     info = source.discover()
     assert info['dtype'] == list(zip(expected_df.columns, expected_df.dtypes))
-    assert info['shape'] == (expected_df.shape[0],)
+    assert info['shape'] == (None,)
     assert info['npartitions'] == 1
 
 
