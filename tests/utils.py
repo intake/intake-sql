bf72eefee6fe02dc6e1d54a7e16a74311a1d4f14
Martin Durant <martin.durant@utoronto.ca>
2021-08-24 09:48:07 -0400

    again

--- tests/utils.py
+++ tests/utils.py
@@ -9,13 +9,13 @@ import sqlalchemy as sa
 df = pd.DataFrame({
     'a': np.random.rand(100),
     'b': np.random.randint(100),
-    'c': np.random.choice(['a', 'b', 'c', 'd'], size=100)
+    'c': np.random.choice(['a', 'b', 'c', 'd'], size=100, dtype="O")
 })
 df.index.name = 'p'
 df2 = pd.DataFrame({
     'd': np.random.rand(100),
     'e': np.random.randint(100),
-    'f': np.random.choice(['a', 'b', 'c', 'd'], size=100)
+    'f': np.random.choice(['a', 'b', 'c', 'd'], size=100 dtype="O")
 })
 
 
